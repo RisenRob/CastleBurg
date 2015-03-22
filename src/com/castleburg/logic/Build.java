1976f77ebec1507bf8e9358423b8c33049e27e3c
krysabel <paha-karelin@mail.ru>
2015-03-22 17:18:02 +0700

    Revert "CastleBurg"
    This reverts commit 1c19f03aeb3316f7e770de72d9b428618ace35c4.

--- src/com/castleburg/logic/Build.java
+++ src/com/castleburg/logic/Build.java
@@ -87,13 +87,13 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			po[1][4]=true;
 			//Подъёмник ,на одно золото менбше при строение из 2 и 3 колоны
 		}
-				if (pos[2][0]==true && player.gold>=2 && player.wood>=1 && player.stone>=2 &&po[2][0]==false && po[1][4]==true){
-					player.gold-=2;
-					player.wood--;
-					player.stone-=2;
-					player.win+=7;
-					po[2][0]=true;//Церковь при битве против демонов +1 в битве
-				} else
+		if (pos[2][0]==true && player.gold>=2 && player.wood>=1 && player.stone>=2 &&po[2][0]==false && po[1][4]==true){
+			player.gold-=2;
+			player.wood--;
+			player.stone-=2;
+			player.win+=7;
+			po[2][0]=true;//Церковь при битве против демонов +1 в битве
+		} else
 		if (pos[2][0]==true && player.gold>=3 && player.wood>=1 && player.stone>=2 &&po[2][0]==false){
 			player.gold-=3;
 			player.wood--;
@@ -101,13 +101,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.win+=7;
 			po[2][0]=true;//Церковь при битве против демонов +1 в битве
 		}
-				if (pos[2][1]==true && player.gold>=1 && player.wood>=3 && player.stone>=1 &&po[2][1]==false && po[1][4]==true){
-					player.gold-=1;
-					player.wood-=3;
-					player.stone--;	
-					player.win+=2;
-					po[2][1]=true;//Фермы в каждом ходу +1 кубик но -1 в битве
-				}else
 		if (pos[2][1]==true && player.gold>=2 && player.wood>=3 && player.stone>=1 &&po[2][1]==false){
 			player.gold-=2;
 			player.wood-=3;
@@ -115,13 +108,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.win+=2;
 			po[2][1]=true;//Фермы в каждом ходу +1 кубик но -1 в битве
 		}
-		if (pos[2][2]==true && player.gold>=1 && player.wood>=2 && player.stone>=1 &&po[2][2]==false && po[1][4]==true){
-			player.gold-=1;
-			player.wood-=2;
-			player.stone--;	
-			player.win+=4;
-			po[2][2]=true;//Казармы в фазе наёма войск платить всего 1 тавар
-		}else
 		if (pos[2][2]==true && player.gold>=2 && player.wood>=2 && player.stone>=1 &&po[2][2]==false){
 			player.gold-=2;
 			player.wood-=2;
@@ -129,13 +115,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.win+=4;
 		    po[2][2]=true;//Казармы в фазе наёма войск платить всего 1 тавар
 		}
-		if (pos[2][3]==true && player.gold>=1 &&  player.stone>=2 &&po[2][3]==false && po[1][4]==true){
-			player.gold-=1;	
-			player.stone-=2;
-			player.war++;
-			player.win+=2;
-			po[2][3]=true;//Каменные стены ,+1 в битве, Победа при равенстве сил+
-		}else
 		if (pos[2][3]==true && player.gold>=2 &&  player.stone>=2 &&po[2][3]==false){
 			player.gold-=2;	
 			player.stone-=2;
@@ -143,13 +122,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.win+=2;
 			po[2][3]=true;//Каменные стены ,+1 в битве, Победа при равенстве сил+
 		}
-		if (pos[2][4]==true && player.gold>=1 && player.wood>=1 && player.stone>=1 &&po[2][4]==false && po[1][4]==true){
-			player.gold-=1;	
-			player.stone--;	
-			player.wood--;	
-			player.win+=2;
-			po[2][4]=true;//Ратуша,можно отдать +2 к кубику или 1 товар что бы получить победное очко
-		}else
 		if (pos[2][4]==true && player.gold>=2 && player.wood>=1 && player.stone>=1 &&po[2][4]==false){
 			player.gold-=2;	
 			player.stone--;	
@@ -157,25 +129,12 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.win+=2;
 			po[2][4]=true;//Ратуша,можно отдать +2 к кубику или 1 товар что бы получить победное очко
 		}
-		if (pos[3][0]==true && player.gold>=4 &&  player.stone>=3 &&po[3][0]==false && po[1][4]==true){
-			player.gold-=4;	
-			player.stone-=3;
-			player.win+=9;
-			po[3][0]=true;//Собор,в концке игры 1 победное очко за каждые 2 товара
-		}else 
 		if (pos[3][0]==true && player.gold>=5 &&  player.stone>=3 &&po[3][0]==false){
 			player.gold-=5;	
 			player.stone-=3;
 			player.win+=9;
 			po[3][0]=true;//Собор,в концке игры 1 победное очко за каждые 2 товара
 		}
-		if (pos[3][1]==true && player.gold>=2 && player.wood>=1 && player.stone>=2 &&po[3][1]==false && po[1][4]==true){
-			player.gold-=2;	
-			player.stone--;	
-			player.wood-=2;	
-			player.win+=4;
-			po[3][1]=true;//Торговая гильдия,+1 золото в начале каждого урожая+
-		}else
 		if (pos[3][1]==true && player.gold>=3 && player.wood>=1 && player.stone>=2 &&po[3][1]==false){
 			player.gold-=3;	
 			player.stone--;	
@@ -183,14 +142,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.win+=4;
 			po[3][1]=true;//Торговая гильдия,+1 золото в начале каждого урожая+
 		}
-		if (pos[3][2]==true && player.gold>=2 && player.wood>=2 && player.stone>=2 && po[3][2]==false && po[1][4]==true){
-			player.war+=2;
-			player.win+=6;
-			player.gold-=2;
-			player.wood-=2;
-			player.stone-=2;
-			po[3][2]=true;//Гильдия магов +2 в битве+
-		}else
 		if (pos[3][2]==true && player.gold>=3 && player.wood>=2 && player.stone>=2 && po[3][2]==false){
 			player.war+=2;
 			player.win+=6;
@@ -199,13 +150,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.stone-=2;
 			po[3][2]=true;//Гильдия магов +2 в битве+
 		}
-		if (pos[3][3]==true && player.gold>=2 &&  player.stone>=2 &&po[3][3]==false && po[1][4]==true){
-			player.gold-=2;	
-			player.stone-=2;
-			player.win+=4;
-			player.war++;
-			po[3][3]=true;//Крепость,+1 в битве,при победе в битве +1 победно очко дополнительно+
-		}else
 		if (pos[3][3]==true && player.gold>=3 &&  player.stone>=2 &&po[3][3]==false){
 			player.gold-=3;	
 			player.stone-=2;
@@ -213,13 +157,6 @@ if (pos[1][1]==true && player.wood>=2 && player.gold>=2 && po[1][1]==false){
 			player.war++;
 			po[3][3]=true;//Крепость,+1 в битве,при победе в битве +1 победно очко дополнительно+
 		}
-		if (pos[3][4]==true && player.gold>=1 && player.wood>=2 && player.stone>=2 && po[3][4]==false && po[1][4]==true){
-			player.win+=4;
-			player.gold-=1;
-			player.wood-=2;
-			player.stone-=2;
-			po[3][4]=true;//Посольство,в конце уражая +1 победное очко+
-		}else
 		if (pos[3][4]==true && player.gold>=2 && player.wood>=2 && player.stone>=2 && po[3][4]==false){
 			player.win+=4;
 			player.gold-=2;
