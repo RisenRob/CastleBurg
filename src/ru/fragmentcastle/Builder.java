5edf5118c76fdcde9f0555828807a61fa52a4c87
RisenRob <clickrob98@gmail.com>
2015-03-31 23:11:21 +0700

    Fragment
    fix

--- src/ru/fragmentcastle/Builder.java
+++ src/ru/fragmentcastle/Builder.java
@@ -48,6 +48,10 @@ public class Builder extends Fragment {
 		image[3][2] = (ImageView)v.findViewById(R.id.imageView12);
 		image[3][3] = (ImageView)v.findViewById(R.id.imageView16);
 		image[3][4] = (ImageView)v.findViewById(R.id.imageView20);
+		/*game.arplayer.sort();
+		game.next_player();
+		game.refresh_players();
+		*/
 		player=game.player;
 		build=player.build;
 		pos=build.pos;
